f01001a66ad74784c3eb6372249967183630b236
Mohamed Arradi-Alaoui( اراذي حال و ي محمد) <arradi.mohamed@gmail.com>
2017-01-27 14:29:25 +0000

    change sign in screen

---
+++ HackingGames/HackingGames/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  HackingGames
+//
+//  Created by MOHAMED ARRADI-ALAOUI on 27/01/2017.
+//  Copyright © 2017 HackerGames. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+   static let blueColor : UIColor = UIColor(red: 18.0 / 255.0, green: 105.0 / 255.0, blue: 240.0 / 255.0, alpha: 1)
+}
